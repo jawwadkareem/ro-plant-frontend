e44ebcb3e112cc642ae127bd0dbdc802e8f80d21
jawwadkareem <karimjawwad09@gmail.com>
2025-06-07 17:20:06 +0500

    Frontend Updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
-const API_BASE_URL ='https://ro-plant-backend-73yj.vercel.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+// const API_BASE_URL ='https://ro-plant-backend-73yj.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
